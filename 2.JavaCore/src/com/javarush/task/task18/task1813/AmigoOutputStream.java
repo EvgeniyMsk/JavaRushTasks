5b18c5a9dbc4d400bce8ad3b093d72f0b4cbbcfb
EvgeniyNotebook <logvinov.evgeniy@yandex.ru>
2019-03-10 19:55:53 +0300

    19:55

--- 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1813/AmigoOutputStream.java
@@ -3,16 +3,53 @@ package com.javarush.task.task18.task1813;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 /* 
 AmigoOutputStream
 */
 
-public class AmigoOutputStream {
+public class AmigoOutputStream extends FileOutputStream{
     public static String fileName = "C:/tmp/result.txt";
-
+    private FileOutputStream fos;
     public static void main(String[] args) throws FileNotFoundException {
         new AmigoOutputStream(new FileOutputStream(fileName));
     }
 
+    public AmigoOutputStream(FileOutputStream fileOutputStream) throws FileNotFoundException {
+        super(fileName);
+        fos = fileOutputStream;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        fos.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        fos.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        fos.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        fos.flush();
+        fos.write("JavaRush © All rights reserved.".getBytes());
+        fos.close();
+    }
+
+    @Override
+    public FileChannel getChannel() {
+        return fos.getChannel();
+    }
+
+    @Override
+    public void flush() throws IOException {
+        fos.flush();
+    }
 }
