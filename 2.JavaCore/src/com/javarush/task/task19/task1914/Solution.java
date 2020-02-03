46d46ebd637e6459d01f7fab394bd48b3a9900db
EvgeniyPC <logvinov.evgeniy@yandex.ru>
2020-02-03 22:39:16 +0300

    My Commit

--- 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
@@ -8,6 +8,9 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        String name = new String("Hello");
+
+        System.out.println(name.toUpperCase());
     }
 
     public static class TestString {
